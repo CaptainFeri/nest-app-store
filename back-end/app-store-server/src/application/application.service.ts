8357e5f32eb162c019bc08657314e55bbc7d2937
mohammad <mh.hsp1377@gmail.com>
2021-10-20 16:39:47 +0330

    add repository of application and applicationDes in application service

--- back-end/app-store-server/src/application/application.service.ts
+++ back-end/app-store-server/src/application/application.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ApplicationEntity } from './entity/application.entity';
+import { ApplicationDesEntity } from './entity/applicationDes.entity';
 
 @Injectable()
-export class ApplicationService {}
+export class ApplicationService {
+    constructor(
+        @InjectRepository(ApplicationEntity)
+        private readonly applicationRepo: Repository<ApplicationEntity>,
+        @InjectRepository(ApplicationDesEntity)
+        private readonly applicationDesRepo: Repository<ApplicationDesEntity>,
+    ) {}
+}

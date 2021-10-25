e2c7ae7d6a6693211cb2f0c641c4ddb77ea47d52
Koorosh Karkehabadi <koorosh.k98@gmail.com>
2021-10-25 14:33:21 +0330

    add cart items placeholder

--- frontend/katibeh/lib/welcome.dart
+++ frontend/katibeh/lib/welcome.dart
@@ -8,19 +8,19 @@ class Welcome extends StatelessWidget {
 
   Welcome({Key? key}) : super(key: key);
 
-  static final Map<int, Color> color = {
-    50: Color.fromRGBO(136, 14, 79, .1),
-    100: Color.fromRGBO(136, 14, 79, .2),
-    200: Color.fromRGBO(136, 14, 79, .3),
-    300: Color.fromRGBO(136, 14, 79, .4),
-    400: Color.fromRGBO(136, 14, 79, .5),
-    500: Color.fromRGBO(136, 14, 79, .6),
-    600: Color.fromRGBO(136, 14, 79, .7),
-    700: Color.fromRGBO(136, 14, 79, .8),
-    800: Color.fromRGBO(136, 14, 79, .9),
-    900: Color.fromRGBO(136, 14, 79, 1),
-  };
-  static MaterialColor colorCustom = MaterialColor(0xFF880E4F, color);
+  // static final Map<int, Color> color = {
+  //   50: Color.fromRGBO(136, 14, 79, .1),
+  //   100: Color.fromRGBO(136, 14, 79, .2),
+  //   200: Color.fromRGBO(136, 14, 79, .3),
+  //   300: Color.fromRGBO(136, 14, 79, .4),
+  //   400: Color.fromRGBO(136, 14, 79, .5),
+  //   500: Color.fromRGBO(136, 14, 79, .6),
+  //   600: Color.fromRGBO(136, 14, 79, .7),
+  //   700: Color.fromRGBO(136, 14, 79, .8),
+  //   800: Color.fromRGBO(136, 14, 79, .9),
+  //   900: Color.fromRGBO(136, 14, 79, 1),
+  // };
+  // static MaterialColor colorCustom = MaterialColor(0xFF880E4F, color);
 
   @override
   Widget build(BuildContext context) {
@@ -29,27 +29,31 @@ class Welcome extends StatelessWidget {
       child: Consumer<ThemeModel>(
         builder: (context, model, _) {
           return MaterialApp(
-            // theme: ThemeData.light(),
             theme: ThemeData(
-              primarySwatch: colorCustom,
-              accentColor: Colors.amber,
-              backgroundColor: Colors.blue,
+              // fontFamily: "IranSans",
+              primarySwatch: Colors.blue,
+              scaffoldBackgroundColor: Colors.white,
             ),
-            darkTheme: ThemeData.dark(),
+            darkTheme: ThemeData(
+              // fontFamily: "IranSans",
+              // primarySwatch: colorCustom,
+              appBarTheme: AppBarTheme(backgroundColor: Colors.grey[850]),
+              scaffoldBackgroundColor: Colors.grey[900],
+              brightness: Brightness.dark,
+            ),
             themeMode: model.mode,
             home: Scaffold(
               appBar: AppBar(
                 title: Text(
-                  'کتیبه',
+                  'Katibeh',
                   style: TextStyle(
-                    fontFamily: 'IranSans',
                     fontSize: 20,
                   ),
                   textDirection: TextDirection.rtl,
                 ),
                 actions: [
                   Padding(
-                    padding: const EdgeInsets.only(right: 20),
+                    padding: const EdgeInsets.only(right: 30),
                     child: GestureDetector(
                       onTap: () => model.toggleMode(),
                       child: Row(
@@ -57,7 +61,6 @@ class Welcome extends StatelessWidget {
                           // Text(
                           //   model.name,
                           //   style: TextStyle(
-                          //       fontFamily: 'IranSans',
                           //       fontSize: 15,
                           //       color: model.color),
                           //   textDirection: TextDirection.rtl,
@@ -65,7 +68,11 @@ class Welcome extends StatelessWidget {
                           // SizedBox(
                           //   width: 5,
                           // ),
-                          Icon(model.icon, color: model.color)
+                          Icon(
+                            model.icon,
+                            color: model.color,
+                            size: 30,
+                          )
                         ],
                       ),
                     ),
@@ -77,145 +84,55 @@ class Welcome extends StatelessWidget {
                 itemBuilder: (content, index) => GestureDetector(
                   onTap: () {},
                   child: Card(
-                    elevation: 6,
-                    color: Colors.amber,
+                    elevation: 2,
+                    color: model.cardColor,
                     margin: const EdgeInsets.all(3),
                     child: Padding(
                       padding: const EdgeInsets.symmetric(
-                          vertical: 15, horizontal: 10),
+                          vertical: 10, horizontal: 10),
                       child: Row(
-                        mainAxisAlignment:
-                        MainAxisAlignment.spaceBetween,
                         children: [
-                          // Column(
-                          //   children: [
-                          //     Padding(
-                          //       padding:
-                          //       const EdgeInsets.symmetric(
-                          //           horizontal: 5),
-                          //       child: const Icon(
-                          //         Icons.alarm,
-                          //         size: 28,
-                          //       ),
-                          //     ),
-                          //   ],
-                          // ),
-                          // Column(
-                          //   crossAxisAlignment:
-                          //   CrossAxisAlignment.start,
-                          //   children: [
-                          //     Row(
-                          //       children: [
-                          //         Text(
-                          //           "به من",
-                          //           style: const TextStyle(
-                          //               fontFamily: 'IranSans'),
-                          //         ),
-                          //         SizedBox(width: 5),
-                          //         Text(
-                          //             alarmContent['others']
-                          //             [index]
-                          //             ['distance']
-                          //                 .toString() +
-                          //                 " کیلومتری",
-                          //             style: const TextStyle(
-                          //                 fontFamily:
-                          //                 'IranSans')),
-                          //       ],
-                          //     ),
-                          //     SizedBox(height: 10),
-                          //     Row(
-                          //       children: [
-                          //         Directionality(
-                          //           textDirection:
-                          //           TextDirection.rtl,
-                          //           child: Text(
-                          //             alarmContent['others'][
-                          //             index]
-                          //             ['status']
-                          //                 .toString() ==
-                          //                 "1"
-                          //                 ? "به مقصد رسیده"
-                          //                 : "به مقصد نرسیده",
-                          //             style: TextStyle(
-                          //                 fontFamily:
-                          //                 'IranSans',
-                          //                 color: alarmContent['others']
-                          //                 [
-                          //                 index]
-                          //                 [
-                          //                 'status']
-                          //                     .toString() ==
-                          //                     "1"
-                          //                     ? Colors.teal
-                          //                     : Colors.red),
-                          //             textAlign:
-                          //             TextAlign.start,
-                          //           ),
-                          //         ),
-                          //       ],
-                          //     ),
-                          //     SizedBox(height: 10),
-                          //     Row(
-                          //       children: [
-                          //         Directionality(
-                          //           textDirection:
-                          //           TextDirection.ltr,
-                          //           child: Text(
-                          //             Utils.calcTime(
-                          //                 alarmContent['others']
-                          //                 [index]
-                          //                 ['updated_at']),
-                          //             style: const TextStyle(
-                          //                 fontFamily:
-                          //                 'IranSans'),
-                          //           ),
-                          //         ),
-                          //       ],
-                          //     )
-                          //   ],
-                          // ),
-                          // Row(
-                          //   crossAxisAlignment:
-                          //   CrossAxisAlignment.end,
-                          //   children: [
-                          //     Container(
-                          //       width: w,
-                          //       child: Text(
-                          //         alarmContent['others'][index]
-                          //         ['city_name'],
-                          //         overflow: TextOverflow.clip,
-                          //         style: const TextStyle(
-                          //             fontFamily: 'IranSans',
-                          //             color: Colors.deepPurple),
-                          //       ),
-                          //     ),
-                          //   ],
-                          // ),
-                          // Column(
-                          //   children: [
-                          //     Switch(
-                          //       value: alarmContent['others']
-                          //       [index]
-                          //       ['active']
-                          //           .toString() ==
-                          //           'false'
-                          //           ? false
-                          //           : (alarmContent['others']
-                          //       [index]
-                          //       ['active']
-                          //           .toString() ==
-                          //           '0'
-                          //           ? false
-                          //           : true),
-                          //       onChanged: (val) async =>
-                          //       await onChangedSwitch(
-                          //           alarmContent['others']
-                          //           [index],
-                          //           val),
-                          //     ),
-                          //   ],
-                          // )
+                          Column(
+                            children: [
+                              Container(
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(50),
+                                    color: Colors.grey[400]),
+                                width: 60,
+                                height: 60,
+                                child: Icon(
+                                  Icons.apps,
+                                  size: 40,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Telegram",
+                                style: TextStyle(
+                                    fontSize: 17, fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(height: 10),
+                              Row(
+                                children: [
+                                  // Directionality(
+                                  //   textDirection: TextDirection.rtl,
+                                  //   child:
+                                  Text(
+                                    "15 MB", style: TextStyle(fontSize: 12),
+                                    // textAlign: TextAlign.start,
+                                    // ),
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
                         ],
                       ),
                     ),
